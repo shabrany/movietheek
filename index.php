c49292ea91ff5ff53112d21094cd86feaf9c83f5
Jorge Fernandez <yoyo.armani@gmail.com>
2017-06-25 22:41:56 +0200

    set two columns display for mobile devices

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ include 'header.php';
                 <a href="edit.php?id=<?php echo $movie['id']; ?>">
             <?php endif; ?>
 
-            <article class="col-sm-2" class="dvd-movie">
+            <article class="col-sm-2 col-xs-6" class="dvd-movie">
                 <div class="poster">
                     <?php if (strlen($movie['poster']) > 10): ?>
                     <img src="<?php echo $movie['poster']; ?>">
