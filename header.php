2a3c2cc5ee815c03dcfb4f06ea3be351ed11af4a
Jorge Fernandez <yoyo.armani@gmail.com>
2017-06-25 22:37:28 +0200

    change index files

--- header.php
+++ header.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<title></title>
 		<meta charset="UTF-8">
+		<meta http-equiv="x-ua-compatible" content="ie=edge">
+		<title>Mijn DVD collectie</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<!-- Latest compiled and minified CSS -->
 		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 		<link rel="stylesheet" type="text/css" href="css/style.css">
 	</head>
 	<body>
 		<div class="container">
-			<h1><a href="index2.php">Movietheek</a></h1>
+			<h1><a href="index.php">Movietheek</a></h1>
 			<main class="content">
--- index2.php
+++ index2.php
@@ -1,51 +1,52 @@
-
 <?php
+
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-
 include  __DIR__ . '/lib/start.php';
 
+// sort by
 $orderBy = filter_input(INPUT_GET, FILTER_SANITIZE_STRING);
+
+// get all movies
 $movies = get_all_movies($orderBy);
 
 include 'header.php';
-
 ?>
 
 <?php if ($config['admin']): ?>
-	<div class="row">
-	    <div class="col-sm-12 text-right">
-	        <a href="add.php" class="btn btn-primary">new item</a>
-	    </div>
-	</div>
-<?php endif ?>
+<div class="row">
+    <div class="col-sm-12 text-right">
+        <a href="add.php" class="btn btn-primary">new item</a>
+    </div>
+</div>
 <br>
+<?php endif; ?>
+
 <h3>My Collection (<?php echo count($movies); ?>)</h3>
 
 <?php if (count($movies)): ?>
-    <div class="row">
-		<?php foreach ($movies as $movie): ?>
-			<?php if ($config['admin']): ?>
-				<a href="edit.php?id=<?php echo $movie['id']; ?>">
-			<?php endif; ?>
-
-			<article class="col-sm-2" class="dvd-movie">
-				<div class="poster">
-					<?php if (strlen($movie['poster']) > 10): ?>
-					<img src="<?php echo $movie['poster']; ?>">
-					<?php endif; ?>
-				</div>
-				<div class="movie-description">
-					<h4 class="title"><?php echo $movie['title']; ?></h4>
-					<div class="year">(<?php echo $movie['year']; ?>)</div>
-				</div>
-			</article>
-
-			<?php if ($config['admin']): ?>
-				</a>
-			<?php endif; ?>
-		<?php endforeach; ?>
-	</div>
-
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th data-sort-type="integer">ID</th>
+                <th>Title</th>
+                <th colspan="2">Authors</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($movies as $movie): ?>
+                <tr>
+                    <td><?php echo $movie['id']; ?></td>
+                    <td><?php echo trim($movie['title']); ?></td>
+                    <td><?php echo trim($movie['authors']); ?></td>
+                    <td class="text-right">
+                        <?php if ($config['admin']): ?>
+                            <a href="edit.php?id=<?php echo $movie['id'] ?>" class="btn btn-warning btn-xs">edit</a>
+                        <?php endif; ?>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
 <?php endif; ?>
 <?php include 'footer.php'; ?>
